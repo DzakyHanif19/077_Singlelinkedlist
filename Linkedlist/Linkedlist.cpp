4f452ce1be669dd790ee2e27fa410302049234f7
DzakyHanif19 <dzakyannafi1@gmail.com>
2023-05-19 17:35:42 +0700

    menghapus node dari dalam list

--- Linkedlist/Linkedlist.cpp
+++ Linkedlist/Linkedlist.cpp
@@ -79,3 +79,16 @@ bool List::listEmpty()
 	else
 		return false;
 }
+bool List::delNode(int nim)/*menghapus node dari dalam list*/
+{
+	Node* current, * previous;
+	if (Search(nim, &previous,&current) == false)
+		return false;
+	previous->next = current->next;
+	if (current == START)
+		START = START->next;
+
+	delete current;
+	return true;
+}
+
